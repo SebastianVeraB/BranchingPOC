823f490a5855ed18dd5dc512bf0d97b2c5054201
Sebastian Vera <42625211+SebastianVeraB@users.noreply.github.com>
2021-02-11 02:15:23 -0300

    Update PipeGovernor.groovy

--- vars/PipeGovernor.groovy
+++ vars/PipeGovernor.groovy
@@ -1,10 +1,4 @@
-def call(Map config=[:]) {
-
-    if (config.allowsToDeployTo == "QA-Merge") {
-        echo "ahhhhhhhhh"
-        return true
-    } else {
-       return false
-    }
-    
+def allowsToDeployTo(org) {
+    echo "called allows to deploy to ${org}"
+    return true
 }
