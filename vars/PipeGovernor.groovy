18cda19cc9e2f0f99923afd8e8144f6fd39877b9
Sebastian Vera <42625211+SebastianVeraB@users.noreply.github.com>
2021-02-11 02:44:18 -0300

    Update PipeGovernor.groovy

--- vars/PipeGovernor.groovy
+++ vars/PipeGovernor.groovy
@@ -2,6 +2,7 @@ def call(Map config=[:]) {
 
     if (config.allowsToDeployTo == "QA-Merge") {
         echo "called allows to deploy to qa merge"
+        echo pullRequest.base
         return true
     } else {
        return false
